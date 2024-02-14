a650eb112bd7aefa2969bc668577e23d2282ded6
Olivier <ocrampette@gmail.com>
2024-02-14 20:23:29 +0100

    yooo

--- spraywall_detection.py
+++ spraywall_detection.py
@@ -1,1 +1,11 @@
-print("hello")
+import cv2 as cv
+from matplotlib import pyplot as plt
+import numpy as np
+img = cv.imread('sprywallclean.png',cv.IMREAD_GRAYSCALE)
+ret,thresh =cv.threshold(img,127, 150,cv.THRESH_BINARY)
+plt.imshow(thresh,'gray',vmin=0,vmax=255)
+plt.xticks([]),plt.yticks([])
+
+
+
+plt.show()
